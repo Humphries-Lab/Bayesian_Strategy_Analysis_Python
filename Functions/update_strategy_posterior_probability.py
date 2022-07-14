412d69dd34fbc89dfba0db3a1d2475e5d930d2f8
Lowri Powell <107247239+lowripowell@users.noreply.github.com>
2022-07-15 00:01:13 +0100

    Add files via upload

---
+++ Functions/update_strategy_posterior_probability.py
@@ -0,0 +1,21 @@
+global trial_type
+
+def update_strategy_posterior_probability(trial_type,decay_rate,success_total,failure_total,alpha0,beta0):
+    if trial_type == "success":
+        success_total = decay_rate * success_total + 1
+        failure_total = decay_rate * failure_total
+        alpha = alpha0 + success_total
+        beta = beta0 + failure_total
+    elif trial_type == "failure":
+        success_total = decay_rate * success_total
+        failure_total = decay_rate * failure_total + 1
+        alpha = alpha0 + success_total
+        beta = beta0 + failure_total
+    else:
+        alpha = None
+        beta = None
+
+    return success_total, failure_total, alpha, beta
+
+
+
