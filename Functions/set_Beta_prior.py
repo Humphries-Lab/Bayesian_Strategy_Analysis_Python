14c83c1a85549da60a92d93b200c35d3304a5eca
Lowri Powell <107247239+lowripowell@users.noreply.github.com>
2022-07-15 02:37:21 +0100

    Update set_Beta_prior.py

--- Functions/set_Beta_prior.py
+++ Functions/set_Beta_prior.py
@@ -1,12 +1,16 @@
 # set requested prior parameters for Beta distributions
 # [A,B]= SET_BETA_PRIOR(P) sets the Beta distribution to the specified prior
 # in P: 'Uniform'; 'Jeffreys'
+global beta0, alpha0
 
 
 def set_priors(prior_type):
-    if prior_type == "Uniform":
-        return 1
-    if prior_type == "Jeffreys":
-        return 0.5
+    if prior_type == 'Uniform':
+        alpha0 = 1
+        beta0 = 1
+    elif prior_type == 'Jeffreys':
+        alpha0 = 0.5
+        beta0 = 0.5
     else:
         print('Unknown prior distribution for the Beta distribution')
+    return alpha0, beta0
