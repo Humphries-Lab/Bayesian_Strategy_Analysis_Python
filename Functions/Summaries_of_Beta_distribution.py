6b60cf2726035bca45f3580a10695f019433d3f6
Lowri Powell <107247239+lowripowell@users.noreply.github.com>
2022-07-18 16:41:03 +0100

    Update Summaries_of_Beta_distribution.py

--- Functions/Summaries_of_Beta_distribution.py
+++ Functions/Summaries_of_Beta_distribution.py
@@ -6,20 +6,22 @@ def summaries_of_Beta_Distribution(Alpha, Beta, type, *args):
     if np.isnan(Alpha) or np.isnan(Beta):
         statistic = nan
     else:
-        match type:
-            case 'MAP':
+        if type == 'MAP':
                 x = np.arange(0, 1, 0.001)
                 y = beta.pdf(x, Alpha, Beta)
                 statistic = x[np.argmax(y)]
-            case 'Mean':
+        elif type == 'Mean':
                 statistic = Alpha / (Alpha + Beta)
-            case 'Var':
+        elif type == 'Var':
                 statistic = ((Alpha * Beta) / (((Alpha + Beta)**2) * (Alpha + Beta + 1)))
-            case 'precision':
+        elif type == 'precision':
                 statistic = 1/((Alpha * Beta) / (((Alpha + Beta) ** 2) * (Alpha + Beta + 1)))
-            case 'Percentile':
+        elif type == 'Percentile':
                 Prct = args
                 Delta = 1 - Prct
                 P = [Delta/2, Prct + Delta/2]
                 statistic = beta.ppf(P, Alpha, Beta)
+        else:
+            print("ERROR")
     return statistic
+
