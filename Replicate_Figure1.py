dd89f9643500e151d82f6708d28c68d0a8d99347
Mark Humphries (staff) <Mark.Humphries@nottingham.ac.uk>
2022-08-23 12:00:07 +0100

    started Figure 1 script edits

--- Replicate_Figure1.py
+++ Replicate_Figure1.py
@@ -15,7 +15,9 @@ from Functions.Summaries_of_Beta_distribution import summaries_of_Beta_Distribut
 TestData = pd.read_csv('data.csv')
 
 #  choose strategies to evaluate: subset shown in Figure 1
+strategies = ['go_left','go_right']
 
+# set prior
 prior_type = 'Uniform' #set prior type
 decay_rate = 0.9
 
@@ -23,5 +25,12 @@ decay_rate = 0.9
 
 no_Trials = np.size(TestData.TrialIndex)
 
-for rows in TestData.itertzuples():
-    TestData.apply(go_right(TestData))
+Output = pd.DataFrame(columns = ['Alpha', 'Beta', 'MAPprobability', 'Precision'])  # empty Dataframe to input data into
+
+for trial in range(len(TestData)):
+    
+    rows_of_data = TestData.iloc[0:trial+1]     # select all rows of data up to the curren trial; is trial+1 as dataframe includes colu
+    for index_strategy in range(len(strategies)):
+       # run current strategy model on data up to current trial 
+       
+       # store results 
--- strategy_models/go_right.py
+++ strategy_models/go_right.py
@@ -1,15 +1,14 @@
-# checks if the subject chose the left option on this trial
+# checks if the subject chose the right-hand option on this trial
 # TYPE = GO_RIGHT(TRIAL_DATA) takes the Table of testdata TRIAL_DATA up to the current trial, and
 # returns the TYPE ("success", 'failure')
-import pandas as pd
-
 
 # Checks only the current trial
 
-
-def go_right(TestData):
-    if TestData.Choice == "right":
+def go_right(rows):
+    nTrials = len(rows);
+     # "at" selects the value at the row/column location in the dataframe
+    if rows.at[nTrials-1,'Choice'] == "right":      # check the current trial's choice
         trial_type = "success"
-    else:
+    elif rows.at[nTrials-1,'Choice'] == "left":
         trial_type = "failure"
     return trial_type
