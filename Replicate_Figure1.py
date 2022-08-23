77049ae2f90a5351aafebac1b5bb1cdb8a3f64a9
Mark Humphries (staff) <Mark.Humphries@nottingham.ac.uk>
2022-08-23 14:01:03 +0100

    fixing multiple strategy script
    solved problems of:
    (a) calling functions dynamically [use getattr(()]
    (b) dynamic storage of results [dict of dataframes]
    (c) dynamic updates of scalars [dict of dicts - like a multi-level struct]

--- Replicate_Figure1.py
+++ Replicate_Figure1.py
@@ -1,12 +1,12 @@
 import numpy as np
 import pandas as pd
-
-
 import matplotlib.pyplot as plt
 
-from strategy_models.go_left import go_left
-from strategy_models.go_right import go_right
-from strategy_models.alternate import alternate
+import strategymodels
+
+#from strategy_models.go_left import go_left
+#from strategy_models.go_right import go_right
+#from strategy_models.alternate import alternate
 from Functions.set_Beta_prior import set_priors
 from Functions.update_strategy_posterior_probability import update_strategy_posterior_probability
 from Functions.Summaries_of_Beta_distribution import summaries_of_Beta_Distribution
@@ -25,12 +25,66 @@ decay_rate = 0.9
 
 no_Trials = np.size(TestData.TrialIndex)
 
-Output = pd.DataFrame(columns = ['Alpha', 'Beta', 'MAPprobability', 'Precision'])  # empty Dataframe to input data into
 
+#%% initialise storage
+Output_collection = {} # empty dict in which to store dataframes
+event_totals = {}   # empty dict to store totals of events for each strategy
+# initialise dataframes
+for index_strategy in range(len(strategies)):
+    Output_collection[strategies[index_strategy]] =  pd.DataFrame(columns = ['Alpha', 'Beta', 'MAPprobability', 'Precision'])  # empty Dataframe to input data into
+    event_totals[strategies[index_strategy]] = {}; # create empty dict for this strategy
+    event_totals[strategies[index_strategy]]['success_total'] = 0;
+    event_totals[strategies[index_strategy]]['failure_total'] = 0;
+
+#%%
 for trial in range(len(TestData)):
     
-    rows_of_data = TestData.iloc[0:trial+1]     # select all rows of data up to the curren trial; is trial+1 as dataframe includes colu
+    rows_of_data = TestData.iloc[0:trial+1]     # select all rows of data up to the curren trial; is trial+1 as dataframe includes colu   
+     
+   #%% 
     for index_strategy in range(len(strategies)):
        # run current strategy model on data up to current trial 
+       strategy_fcn = getattr(strategymodels,strategies[index_strategy])  # dynamically assign string as function to be called
+       trial_type = strategy_fcn(rows_of_data)   #call currently assigned function
+     
+       # update probability of strategy
+       [event_totals[strategies[index_strategy]]['success_total'], event_totals[strategies[index_strategy]]['failure_total'], Alpha, Beta] \
+           = update_strategy_posterior_probability(trial_type, decay_rate,event_totals[strategies[index_strategy]]['success_total'], event_totals[strategies[index_strategy]]['failure_total'],alpha0, beta0)
+               
+       MAPprobability = summaries_of_Beta_Distribution(Alpha, Beta, 'MAP')
+       precision = summaries_of_Beta_Distribution(Alpha, Beta, 'precision')
+    
+       # store results  - dynamically-defined dataframe...
+       new_row = {'Alpha':Alpha, 'Beta':Beta, 'MAPprobability':MAPprobability, 'Precision':precision}     # create new row for dataframe as a dict
+       new_df= pd.DataFrame([new_row])   # have to convert to dataframe to use concat!!
+       Output_collection[strategies[index_strategy]] = pd.concat([Output_collection[strategies[index_strategy]], new_df], ignore_index=True)       # add new row to dataframe
        
-       # store results 
+#%% plot results
+# plotting time series of MAPprobability
+plt.figure(figsize=(10, 5))
+plt.plot(Output_collection['go_right'].MAPprobability, linewidth=0.75)  # plots the time series
+plt.axis([0, no_Trials, 0, 1.25])  # establishes axis limits
+plt.xlabel('Trials'), plt.ylabel('P(Strategy)')  # labelling the axis
+plt.axhline(y=0.5, color='firebrick', linestyle='--', linewidth=0.75,
+            label="Chance")  # shows the line at which Chance is exceeded
+sessionLines = TestData[TestData['NewSessionTrials'] == 1].index  # indices list  when new session was started
+plt.vlines(sessionLines, 0, 1, colors='lightgray', linestyles='--', linewidth=0.75,
+           label="New Sessions")  # vertical lines indicate the new session trials
+ruleLines = np.array(TestData[TestData['RuleChangeTrials'] == 1].index)  # indices list  when new session was started
+ruleLines = np.insert(ruleLines, 0, 0)  # sets array for x values of rule change
+
+for x in ruleLines:  # to get shade change for rule change and labels
+    minx = x / no_Trials
+    plt.axhspan(1, 1.25, xmin=minx, alpha=0.3, edgecolor='k')  # change transparency for separation
+    plt.axvline(x, 0.8, 1)  # dividing lines
+
+#  creating labels
+plt.text(1, 1.125, "Right Arm", label='Go to the Right')
+plt.text(120, 1.125, "Lit Arm", label='Go to the Lit Arm')
+plt.text(225, 1.125, "Left Arm", label='Go to the Left')
+plt.text(330, 1.125, "Unlit Arm", label='Go to the Dark Arm')
+plt.text(150, 1.3, "Rule for Reward")
+plt.legend()  # add legend
+plt.show()       
+       
+       
---
+++ strategymodels.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Strategy models module
+
+A set of functions that define each strategy model
+Each function takes a Pandas dataframe, where each row is the data for one trial, 
+up to the current trial
+
+The column names in the dataframe are used to find the values 
+
+Created on Tue Aug 23 12:42:13 2022
+
+@author: lpzmdh
+"""
+
+def go_left(rows):
+    # checks if the subject chose the left option on this trial
+    nTrials = len(rows);
+     # "at" selects the value at the row/column location in the dataframe
+    if rows.at[nTrials-1,'Choice'] == "left":      # check the current trial's choice
+        trial_type = "success"
+    elif rows.at[nTrials-1,'Choice'] == "right":
+        trial_type = "failure"
+    return trial_type
+
+
+def go_right(rows):
+    # checks if the subject chose the right-hand option on this trial
+    nTrials = len(rows);
+     # "at" selects the value at the row/column location in the dataframe
+    if rows.at[nTrials-1,'Choice'] == "right":      # check the current trial's choice
+        trial_type = "success"
+    elif rows.at[nTrials-1,'Choice'] == "left":
+        trial_type = "failure"
+    return trial_type
+
+
+
