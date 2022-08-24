5ceeef0accde498c100574858e9262857c932fc9
mdhumphries <drmdhumphries+git@gmail.com>
2022-08-24 11:57:11 +0100

    updated Demonstrate script

--- Demonstrate_Bayesian_Strategy_Analysis.py
+++ Demonstrate_Bayesian_Strategy_Analysis.py
@@ -4,10 +4,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import strategymodels
+
 from Functions.set_Beta_prior import set_priors
-from strategy_models.go_left import go_left
 from Functions.update_strategy_posterior_probability import update_strategy_posterior_probability
 from Functions.Summaries_of_Beta_distribution import summaries_of_Beta_Distribution
+from Functions.plotSessionStructure import plotSessionStructure
 
 # initiate TestData variable so that rat 2 testdata can be loaded
 TestData = pd.read_csv('data.csv')
@@ -18,14 +20,7 @@ prior_type = "Uniform"  # choosing prior type
 
 decay_rate = 0.9  # Set Decay rate (gamma)
 
-# MAIN LOOP: FOR EACH TRIAL TO UPDATE STRATEGY INDEX
-
 no_Trials = np.size(TestData.TrialIndex)
-
-Alpha = 0   # initialises storage
-Beta = 0 
-MAPprobability = 0 
-precision = 0
 
 success_total = 0  # initialise variables to zero
 failure_total = 0
@@ -35,7 +30,7 @@ Output = pd.DataFrame(columns = ['Alpha', 'Beta', 'MAPprobability', 'Precision']
 #%% run strategy analysis
 for trial in range(len(TestData)):
     rows_of_data = TestData.iloc[0:trial+1]     # select all rows of data up to the curren trial; is trial+1 as dataframe includes column row as row 0????
-    trial_type = go_left(rows_of_data)          # test whether go-left was used       
+    trial_type = strategymodels.go_left(rows_of_data)          # test whether go-left was used       
         
     [success_total, failure_total, Alpha, Beta] = update_strategy_posterior_probability(trial_type, decay_rate,
                                                                                         success_total, failure_total,
@@ -58,22 +53,7 @@ plt.axis([0, no_Trials, 0, 1.25])  # establishes axis limits
 plt.xlabel('Trials'), plt.ylabel('P(Strategy)')  # labelling the axis
 plt.axhline(y=0.5, color='firebrick', linestyle='--', linewidth=0.75,
             label="Chance")  # shows the line at which Chance is exceeded
-sessionLines = TestData[TestData['NewSessionTrials'] == 1].index  # indices list  when new session was started
-plt.vlines(sessionLines, 0, 1, colors='lightgray', linestyles='--', linewidth=0.75,
-           label="New Sessions")  # vertical lines indicate the new session trials
-ruleLines = np.array(TestData[TestData['RuleChangeTrials'] == 1].index)  # indices list  when new session was started
-ruleLines = np.insert(ruleLines, 0, 0)  # sets array for x values of rule change
 
-for x in ruleLines:  # to get shade change for rule change and labels
-    minx = x / no_Trials
-    plt.axhspan(1, 1.25, xmin=minx, alpha=0.3, edgecolor='k')  # change transparency for separation
-    plt.axvline(x, 0.8, 1)  # dividing lines
+plotSessionStructure(TestData)
 
-#  creating labels
-plt.text(1, 1.125, "Right Arm", label='Go to the Right')
-plt.text(120, 1.125, "Lit Arm", label='Go to the Lit Arm')
-plt.text(225, 1.125, "Left Arm", label='Go to the Left')
-plt.text(330, 1.125, "Unlit Arm", label='Go to the Dark Arm')
-plt.text(150, 1.3, "Rule for Reward")
-plt.legend()  # add legend
 plt.show()
